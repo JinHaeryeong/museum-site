6207e852004dc1ff0500fbe1850826f44bdf642e
HaeRyeong Jin <jhr5559@gmail.com>
2025-09-01 12:48:32 +0900

    Feat: 전체 UI 틀 잡기

--- museum-app/src/pages/Home.jsx
+++ museum-app/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import MainContent from "../components/home/MainContent";
 
 export default function Home() {
     return (
         <div>
-            <h1>Home입니다</h1>
+            <MainContent />
         </div>
     );
 }
