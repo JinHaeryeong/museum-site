4e47e4c821d30fdb454b961abd9ce75a4a7ba492
palsa84 <rlawnsghs322.naver.com>
2025-09-01 16:44:54 +0900

    docs: ReservaionInfoCard 코드 간략화

--- museum-app/src/components/reservation/ReservationInfoCard.jsx
+++ museum-app/src/components/reservation/ReservationInfoCard.jsx
@@ -1,37 +1,26 @@
 import React from 'react';
 import '../../assets/styles/reservationInfoCard.css';
+
 export default function ReservationInfoCard({ title, name, email, phone, date, time, guests }) {
+    const reservationData = [
+        { label: '전시명', value: title },
+        { label: '예약자명', value: name },
+        { label: '이메일', value: email },
+        { label: '전화번호', value: phone },
+        { label: '관람 날짜', value: date },
+        { label: '관람 시간', value: time },
+        { label: '인원 수', value: `${guests}명` },
+    ];
+
     return (
         <div className="reservation-info-card">
             <h3 className="reservation-info-title">최종 예약정보 확인</h3>
-            <div className="reservation-info-item">
-                <span className="reservation-title">전시명:</span>
-                <span className="reservation-content">{title}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">예약자명:</span>
-                <span className="reservation-content">{name}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">이메일:</span>
-                <span className="reservation-content">{email}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">전화번호:</span>
-                <span className="reservation-content">{phone}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">관람 날짜:</span>
-                <span className="reservation-content">{date}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">관람 시간:</span>
-                <span className="reservation-content">{time}</span>
-            </div>
-            <div className="reservation-info-item">
-                <span className="reservation-title">인원 수:</span>
-                <span className="reservation-content">{guests}명</span>
-            </div>
+            {reservationData.map((item, idx) => (
+                <div className="reservation-info-item" key={idx}>
+                    <span className="reservation-title">{item.label}:</span>
+                    <span className="reservation-content">{item.value}</span>
+                </div>
+            ))}
         </div>
     );
 }
