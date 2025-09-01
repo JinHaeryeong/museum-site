5e9889235083ef26116b97cf0cc1cc9e364b35c0
HaeRyeong Jin <jhr5559@gmail.com>
2025-09-02 08:20:03 +0900

    merge 전 pull

--- museum-app/src/App.jsx
+++ museum-app/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { ArrowBigDown, Camera } from 'lucide-react';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/styles/global.css";
+import { ArrowBigDown, Camera } from "lucide-react";
 
 function App() {
     return (
-        <div className="container">
+        <div className='container'>
             <BrowserRouter>
                 <div>
                     <Header />
                 </div>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/info" element={<Info />} />
-                    <Route path="/exhibitions" element={<Exhibitions />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/signup" element={<SignUp />} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/info' element={<Info />} />
+                    <Route path='/exhibitions' element={<Exhibitions />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/signup' element={<SignUp />} />
                 </Routes>
             </BrowserRouter>
             <hr />
