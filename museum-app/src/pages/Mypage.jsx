2fae63255792cadfe0decb02fac5aae8944b90d9
palsa84 <suom4@naver.com>
2025-09-02 14:54:37 +0900

    test coomit

---
+++ museum-app/src/pages/Mypage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyOrder from '../components/home/MyOrder';
+
+export default function Mypage() {
+    return (
+        <div>
+            <MyOrder />
+        </div>
+    );
+}
