846ddbd5a9d9dfe92477e483a084ef58fb8d68a7
HaeRyeong Jin <jhr5559@gmail.com>
2025-09-03 12:37:14 +0900

    Docs: 누락된 파일 추가

--- museum-app-server/src/routes/signRouter.js
+++ museum-app-server/src/routes/signRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const signController = require("../controllers/signController");
+const verifyMiddleware = require("../middlewares/verifyMiddleware");
 
 router.post("/signin", signController.signin);
 router.post("/signout", signController.signout);
-router.get("/user", signController.getAuthenticUser);
+router.get("/user", verifyMiddleware.verifyAccessToken, signController.getAuthenticUser); // accessToken이 유효한지 검증하는 미들웨어 작성
+router.post("/refresh", signController.refreshVerify);
 
 module.exports = router;
